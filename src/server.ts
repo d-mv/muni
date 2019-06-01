23781e2aff6151a9a6f9243ad64be4b6f0fb9e7d
d-mv <d.dash.mv@gmail.com>
2019-06-01 14:39:51 +0300

    Cleanup of package.json & fixing paths in server

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,50 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-import * as mongodb from 'mongodb';
-import * as assert from 'assert';
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+import * as mongodb from "mongodb";
+import * as assert from "assert";
 
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 // import compression from "compression";
-const compression = require('compression');
+const compression = require("compression");
 
 // routes
 // import router from '../routes';
-import apiRouter from '../routes/api_router';
-import userRouter from '../routes/user_router';
-import locationRouter from '../routes/location_router';
-import postRouter from '../routes/post_router';
+import apiRouter from "../routes/api_router";
+import userRouter from "../routes/user_router";
+import locationRouter from "../routes/location_router";
+import postRouter from "../routes/post_router";
 
 const dotEnv = dotenv.config();
 const app = express();
 
 app.use(compression());
-// app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/user', userRouter);
-app.use('/api/location', locationRouter);
-app.use('/api/post', postRouter);
-app.use('/api', apiRouter);
+app.use("/api/user", userRouter);
+app.use("/api/location", locationRouter);
+app.use("/api/post", postRouter);
+app.use("/api", apiRouter);
 // app.use('/', router);
 
 // * React
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../../client/build")));
 
 //production mode
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(
+    express.static(path.join(__dirname, "../../client/build/index.html"))
+  );
   //
-  app.get("/index.html", (req:any, res:any) => {
-    res.sendfile(path.join((__dirname = "../client/build/index.html")));
+  app.get("/index.html", (req: any, res: any) => {
+    console.log(path.join(__dirname, "../../client/build/index.html"));
+    res.sendFile(path.join(__dirname,"../../client/build/index.html"));
   });
 }
 
@@ -53,21 +55,19 @@ app.get("/index.html", (req: any, res: any) => {
 
 // * end of React
 
-
-
 // set up db
 // Connection URL
-const defaultUrl = 'mongodb://localhost:27017';
+const defaultUrl = "mongodb://localhost:27017";
 const connectUrl = `${process.env.MONGO_URL || defaultUrl}?retryWrites=true`;
 // Database Name
-const dbName = 'muni';
+const dbName = "muni";
 // Create a new MongoClient
 const MongoClient = mongodb.MongoClient;
 const client = new MongoClient(connectUrl, { useNewUrlParser: true });
 // Use connect method to connect to the Server
 const connect = client.connect(err => {
   assert.equal(null, err);
-  console.log('Connected successfully to DB server');
+  console.log("Connected successfully to DB server");
 
   const db = client.db(dbName);
 
