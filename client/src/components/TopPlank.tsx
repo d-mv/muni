87fea0f8aedc5fca59d4fb774b534599431746dc
d-mv <d.dash.mv@gmail.com>
2019-06-05 21:56:21 +0300

    Top plank is done

--- client/src/components/TopPlank.tsx
+++ client/src/components/TopPlank.tsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
 
-import Button from './Button'
+import { AppState } from "../store";
+import { indexedObj, indexedObjAny } from "../store/types";
+import Button from "./Button";
+import Help from "../icons/Help";
+import Edit from "../icons/Edit";
+import PageTitle from "./PageTitle";
 
-import style from '../styles/TopPlank.module.scss'
+import style from "../styles/TopPlank.module.scss";
 
-const TopPlank = () => {
-  const mockFN = () =>{}
+const TopPlank = (props: {
+  language: indexedObj;
+  locationData: indexedObjAny;
+}) => {
+  const { text, direction } = props.language;
+
+  const mockFN = () => {};
+  const title: string = props.locationData.name[props.language.short]
+    ? props.locationData.name[props.language.short]
+    : props.locationData.name["en"];
   return (
-    <div className={style.plank}>
-      <Button action={mockFN}>?</Button>
-      <h1>Page title</h1>
-      <Button>edit</Button>
-    </div>
+    <header className={direction === "rtl" ? style.plankRight : style.plank}>
+      <Button action={mockFN}>
+        <Help color='primary' />
+      </Button>
+      <PageTitle title={title} direction={direction} />
+      <Button action={mockFN}><Edit color='primary' /></Button>
+    </header>
   );
-}
+};
 
-export default TopPlank
+const mapStateToProps = (state: AppState) => {
+  return {
+    language: state.language,
+    locationData: state.locationData
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(TopPlank);
