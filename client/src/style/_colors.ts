2b36f20289b1630f1689d731f6d9137b40b730d5
d-mv <d.dash.mv@gmail.com>
2019-06-19 22:27:30 +0300

    attention color changed

--- client/src/style/_colors.ts
+++ client/src/style/_colors.ts
@@ -2,10 +2,10 @@
  * Defining the colors, required for inline style
  */
 export const colorsArray: { [index: string]: string } = {
-  primary: "#478FDF",
-  secondary: "#379B37",
-  white: "#fff",
-  dark: "#444",
-  grey: "#777",
-  attention: "#FF072E"
-};
+         primary: "#478FDF",
+         secondary: "#379B37",
+         white: "#fff",
+         dark: "#444",
+         grey: "#777",
+         attention: "#E33039"
+       };
