f537f4d4dda395918232f8df90a9d5a0d48aca68
d-mv <d.dash.mv@gmail.com>
2019-06-19 22:24:56 +0300

    color changed

--- client/src/style/_colors.ts
+++ client/src/style/_colors.ts
@@ -3,7 +3,7 @@
  */
 export const colorsArray: { [index: string]: string } = {
   primary: "#478FDF",
-  secondary: "#03AD24",
+  secondary: "#379B37",
   white: "#fff",
   dark: "#444",
   grey: "#777",
