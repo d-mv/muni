0fa22ddcf31bb71aa3b4a964e62651dce6787493
d-mv <d.dash.mv@gmail.com>
2019-06-22 14:45:01 +0300

    done

--- client/src/features/Post/PostMuni.tsx
+++ client/src/features/Post/PostMuni.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { AppState } from "../../store";
+import { setModule } from "../../store/users/actions";
 import { indexedObjAny } from "../../store/types";
 
 import { Photo, Link, Text } from "./components";
@@ -9,26 +10,91 @@ import { Photo, Link, Text } from "./components";
 import Block from "../../layout/Block";
 
 import style from "./style/Post.module.scss";
+import Content from "../../layout/Content";
+import Header from "../../components/Header";
+import { goBack } from "../../icons";
 
-const PostMuni = (props: { post: any; language: indexedObjAny }) => {
+const PostMuni = (props: {
+  post: any;
+  language: indexedObjAny;
+  location: indexedObjAny;
+  setModule: (arg0: string) => void;
+  prevModule: string;
+}) => {
   const { post } = props;
   const { direction } = props.language;
-console.log(props)
+
+  const goHome = () => {
+    props.setModule(props.prevModule);
+  };
+  const headerObject = {
+    name: props.location.name[props.language.short],
+    left: { icon: goBack("primary"), action: goHome }
+  };
+
   return (
-    <div className={style.post}>
-      <Photo src={post.photo} />
-      <Block>
-        <Link primary text={post.link} direction={direction} />
-      </Block>
-      <Text muni text={post.text} direction={direction} /> */}
-    </div>
+    <Content header>
+      <Header {...headerObject} />
+      <div className={style.wrapper}>
+        <div data-testid='post__view' id={post._id} className={style.post}>
+          <Photo src={post.photo} />
+          <Block>
+            <Link primary text={post.link} direction={direction} />
+          </Block>
+          <Text muni text={post.text} direction={direction} />
+        </div>
+      </div>
+    </Content>
   );
+
+  // <Content header>
+  //   <Header {...headerObject} />
+  //   <div className={style.wrapper}>
+  //     <div data-testid='post__view' id={_id} className={style.post}>
+  //       <TopBlock
+  //         category={category}
+  //         title={title}
+  //         numbersLine={numbersLine}
+  //       />
+  //       <Photo src={photo} edit={mockEdit} />
+  //       <Link primary text={link} direction={direction} edit={mockEdit} />
+  //       <div className={showStyle}>
+  //         <Text
+  //           step
+  //           title={text["post.problem"]}
+  //           text={problem}
+  //           direction={direction}
+  //         />
+  //         <Text
+  //           back
+  //           title={text["post.solution"]}
+  //           text={solution}
+  //           direction={direction}
+  //         />
+  //       </div>
+  //       <ShowMore
+  //         color='primary'
+  //         title={showMoreLessText}
+  //         direction={direction}
+  //         opened={textOpened}
+  //         action={setTextOpened}
+  //       />
+  //     </div>
+  //     <div className={style.voted}>{voteButton}</div>
+  //     {modal}
+  //     {newReplyButton}
+  //     {newReplyComponent}
+  //     {ReplyMessage}
+  //     {setOfThumbs}
+  //   </div>
+  // </Content>;
 };
 
 const mapStateToProps = (state: AppState) => {
   return {
     language: state.language,
-
+    location: state.locationData,
+    prevModule: state.prevModule,
     post: state.locationData.municipality.filter(
       // @ts-ignore
       (post: any) => post._id === state.post._id
@@ -38,5 +104,5 @@ const mapStateToProps = (state: AppState) => {
 
 export default connect(
   mapStateToProps,
-  {}
+  { setModule }
 )(PostMuni);
