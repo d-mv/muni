9281f3f7397cbef03819943fcd953cef1e242b6e
d-mv <d.dash.mv@gmail.com>
2019-07-13 14:17:53 +0300

    Turn off seed

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,15 +1,15 @@
-import dbSeed from "../../db/db_seed";
+// import dbSeed from "../../db/db_seed";
 
 const express = require("express");
 const router = new express.Router();
 
-router.get("/seed", async (req: any, res: any) => {
-  try {
-    const seed = await dbSeed();
-    res.status(201).send({ message: seed });
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
+// router.get("/seed", async (req: any, res: any) => {
+//   try {
+//     const seed = await dbSeed();
+//     res.status(201).send({ message: seed });
+//   } catch (e) {
+//     res.status(400).send(e);
+//   }
+// });
 
 export default router
